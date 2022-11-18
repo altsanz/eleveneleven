b45c7c83b8e5a9f2e254c1855b34272236e3274f
Alejandro Sanz <asanz91@gmail.com>
2022-11-18 18:49:49 +0100

    allow pressing spacebar to replay

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import NumberWrapper from "./components/NumberWrapper";
-const MAX_NUMBER = 9999;
+const MAX_NUMBER = 100;
 const generateRandomNumber = (max: number) => Math.round(Math.random() * max);
 function App() {
+  const [utterance, setUtterance] = React.useState<SpeechSynthesisUtterance>();
   const timerRef = React.useRef<ReturnType<typeof setTimeout>>();
   const [letsGo, setLetsGo] = React.useState(false);
   const [number, setNumber] = React.useState(() =>
@@ -11,27 +12,52 @@ function App() {
   );
 
   React.useEffect(() => {
+    const timeout =
+      letsGo && utterance
+        ? setTimeout(() => {
+            window.speechSynthesis.speak(utterance);
+          }, 200)
+        : undefined;
+    return () => {
+      clearTimeout(timeout);
+      window.speechSynthesis.cancel();
+    };
+  }, [utterance, letsGo]);
+
+  React.useEffect(() => {
     const utterance = new SpeechSynthesisUtterance(number.toString());
     utterance.rate = 0.8;
     utterance.lang = "de-DE";
-    letsGo && setTimeout(() => window.speechSynthesis.speak(utterance));
-    return () => window.speechSynthesis.cancel();
+    setUtterance(utterance);
   }, [letsGo, number]);
 
+  const replayOnSpace = React.useCallback(
+    (ev: KeyboardEvent) => {
+      if (ev.code === "Space" && letsGo && utterance) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        window.speechSynthesis.speak(utterance);
+      }
+    },
+    [letsGo, utterance]
+  );
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", replayOnSpace, false);
+    return () => document.removeEventListener("keydown", replayOnSpace);
+  }, [replayOnSpace]);
+
   const renewNumber = () => {
-    timerRef.current = setTimeout(
-      () => setNumber(generateRandomNumber(MAX_NUMBER)),
-      1000
-    );
+    timerRef.current && clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      let newNumber = number;
+      while (newNumber === number) {
+        newNumber = generateRandomNumber(MAX_NUMBER);
+      }
+      return setNumber(newNumber);
+    }, 1000);
   };
 
-  React.useEffect(
-    () => () => {
-      timerRef.current && clearTimeout(timerRef.current);
-    },
-    []
-  );
-  console.log({ number });
   return (
     <div className="App h-screen">
       {letsGo && <NumberWrapper number={number} onComplete={renewNumber} />}
