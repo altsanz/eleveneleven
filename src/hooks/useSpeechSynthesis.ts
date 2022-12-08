6557e1fa6bc5e64e37044e6bd1d6930fcaaf1c23
Alejandro Sanz <asanz91@gmail.com>
2022-12-08 20:14:35 +0100

    fix iphone not using german voice

--- src/hooks/useSpeechSynthesis.ts
+++ src/hooks/useSpeechSynthesis.ts
@@ -1,4 +1,5 @@
 import React from "react";
+const voicesList = window.speechSynthesis.getVoices();
 
 export const UseSpeechSynthesis = () => {
   const [text, setText] = React.useState<string>();
@@ -7,6 +8,10 @@ export const UseSpeechSynthesis = () => {
   React.useEffect(() => {
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.rate = 0.8;
+    // Without the following line, voice remains in English on iOS
+    // voicesList is here the retured value of speechSynthesis.getVoices()
+    utterance.voice =
+      voicesList.find((voice) => voice.lang === "de-DE") ?? null;
     utterance.lang = "de-DE";
     setUtterance(utterance);
   }, [text]);
