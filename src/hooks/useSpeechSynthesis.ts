5a244e2557d9b8260aa79b479b5231d208284d32
Alejandro Sanz <asanz91@gmail.com>
2022-12-08 22:22:01 +0100

    select specific voice for iPhone

--- src/hooks/useSpeechSynthesis.ts
+++ src/hooks/useSpeechSynthesis.ts
@@ -1,5 +1,9 @@
 import React from "react";
 const voicesList = window.speechSynthesis.getVoices();
+const ua = window.navigator.userAgent;
+const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+const webkit = !!ua.match(/WebKit/i);
+const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
 export const UseSpeechSynthesis = () => {
   const [text, setText] = React.useState<string>();
@@ -10,8 +14,16 @@ export const UseSpeechSynthesis = () => {
     utterance.rate = 0.8;
     // Without the following line, voice remains in English on iOS
     // voicesList is here the retured value of speechSynthesis.getVoices()
-    utterance.voice =
-      voicesList.find((voice) => voice.lang === "de-DE") ?? null;
+
+    if (iOSSafari) {
+      utterance.voice =
+        voicesList.find(
+          (voice) => voice.voiceURI === "com.apple.voice.compact.de-DE.Anna"
+        ) ?? null;
+    } else {
+      utterance.voice =
+        voicesList.find((voice) => voice.lang === "de-DE") ?? null;
+    }
     utterance.lang = "de-DE";
     setUtterance(utterance);
   }, [text]);
